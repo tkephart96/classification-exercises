ca4618c5180dea53e66aa916d59d52942c9d408f
Tyler Kephart <tkephart96@gmail.com>
2023-04-14 16:58:05 -0500

    updated prep

--- prepare.py
+++ prepare.py
@@ -18,14 +18,8 @@ def prep_iris(iris):
     iris = iris.rename(columns={'species_name':'species'})
     dummy_iris = pd.get_dummies(iris.species, drop_first=True)
     iris = pd.concat([iris, dummy_iris], axis=1)
-    # split
-    train_validate, test = train_test_split(iris, test_size=.2, random_state=42, stratify=iris.species)
-    train, validate = train_test_split(train_validate, 
-                                        test_size=.25, 
-                                        random_state=42, 
-                                        stratify=train_validate.species)
-    print('data cleaned, prepped, and split')
-    return train, validate, test
+    print('data cleaned and prepped')
+    return iris
 
 def prep_titanic(titanic):
     """
@@ -40,14 +34,8 @@ def prep_titanic(titanic):
     titanic['embarked'] = titanic.embarked.fillna(value='S')
     dummy_titanic = pd.get_dummies(titanic[['sex','embarked']], drop_first=True)
     titanic = pd.concat([titanic, dummy_titanic], axis=1)
-    # split
-    train_validate, test = train_test_split(titanic, test_size=.2, random_state=42, stratify=titanic.survived)
-    train, validate = train_test_split(train_validate, 
-                                        test_size=.25, 
-                                        random_state=42, 
-                                        stratify=train_validate.survived)
-    print('data cleaned, prepped, and split')
-    return train, validate, test
+    print('data cleaned and prepped')
+    return titanic
 
 def prep_telco(telco):
     """
@@ -68,11 +56,30 @@ def prep_telco(telco):
     telco_obj = telco.select_dtypes(include='object').columns.to_list()
     dummy_telco = pd.get_dummies(telco[telco_obj], drop_first=True)
     telco = pd.concat([telco, dummy_telco], axis=1)
-    # split
-    train_validate, test = train_test_split(telco, test_size=.2, random_state=42, stratify=telco.churn)
+    print('data cleaned and prepped')
+    return telco
+
+def split_data(df, strat, test=.2, validate=.25):
+    """
+    The function splits a given dataframe into training, validation, and test sets based on a specified
+    stratification variable.
+    
+    :param df: The input dataframe that needs to be split into train, validate, and test sets
+    :param strat: The name of the column in the dataframe that will be used for stratified sampling
+    during the data split
+    :param test: The proportion of the data that should be allocated to the test set. In this case, it
+    is set to 0.2 or 20% of the data
+    :param validate: The proportion of the data that will be used for validation. It is set to 0.25,
+    which means that 25% of the data will be used for validation
+    :return: three dataframes: train, validate, and test.
+    """
+    print('data split')
+    train_validate, test = train_test_split(df, test_size=test, random_state=42, stratify=df[{strat}])
     train, validate = train_test_split(train_validate, 
-                                        test_size=.25, 
+                                        test_size=validate, 
                                         random_state=42, 
-                                        stratify=train_validate.churn)
-    print('data cleaned, prepped, and split')
+                                        stratify=train_validate[{strat}])
+    print(f'train -> {train.shape}; {round(len(train)*100/len(df),2)}%')
+    print(f'validate -> {validate.shape}; {round(len(validate)*100/len(df),2)}%')
+    print(f'test -> {test.shape}; {round(len(test)*100/len(df),2)}%')
     return train, validate, test
